a3a42287f7a5351b767a6d6703e3ef6537b8da70
Harshith Vyas <harshithvyas@gmail.com>
2025-09-24 08:12:06 +0530

    some message

---
+++ 04-conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+$PERSON1=$1
+$PERSON2=$2
+
+ echo"$PERSON1::Welcome to Alaska $PERSON2"
+ echo"$PERSON2::Thnk you $PERSON1, finally not on zoom webinar"
+ echo"$PERSON1::So, do we have a deal"
+ echo"$PERSON2::Yes,if you make Ukraine joins russia"
