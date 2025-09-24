54c0642a29ce3b00ce6eece6458350a823d6344a
Harshith Vyas <harshithvyas@gmail.com>
2025-09-24 08:39:49 +0530

    some message

--- 04-conversation.sh
+++ 04-conversation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-$PERSON1=$1
-$PERSON2=$2
+PERSON1=$1
+PERSON2=$2
 
- echo"$PERSON1::Welcome to Alaska $PERSON2"
- echo"$PERSON2::Thnk you $PERSON1, finally not on zoom webinar"
- echo"$PERSON1::So, do we have a deal"
- echo"$PERSON2::Yes,if you make Ukraine joins russia"
+echo "$PERSON1: Welcome to Alaska $PERSON2"
+echo "$PERSON2: Thank you $PERSON1, finally not on Zoom webinar"
+echo "$PERSON1: So, do we have a deal?"
+echo "$PERSON2: Yes, if you make Ukraine join Russia"
