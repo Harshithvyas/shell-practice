66b4d787b5301a8edb060b9659b839542ec5e08b
Harshith Vyas <harshithvyas@gmail.com>
2025-09-29 12:43:35 +0530

    session-06

--- 19-set.sh
+++ 19-set.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+error(){
+    echo "There is an error"
+}
+
+trap error ERR
+
 echo "Hello.."
 echo "Before error.."
-ccaffjl;dnfecho "After error"
+ccaffjl;dnfecho # here shell understands there is an error and signal is ERR 
+echo "After error"
