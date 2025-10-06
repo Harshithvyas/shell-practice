9fa1a1bd851501707eaa88de7f4e45b772aff589
Harshith Vyas <harshithvyas@gmail.com>
2025-10-06 08:28:52 +0530

    session-19

--- 24-backup.sh
+++ 24-backup.sh
@@ -7,22 +7,24 @@ Y="\e[33m"
 N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-script"
-SCRIPT_NAME=$( echo $0 | cut -d "." -fi )
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME%.sh.log"
-
-mkdir -p "$LOGS_FOLDER"
-echo -e "Script started executed at: $(date) " | tee -a "$LOG_FILE"
+SCRIPT_NAME=$(basename "$0" .sh)
+LOG_FILE="$LOGS_FOLDER/${SCRIPT_NAME}.log"
 
 if [ $USERID -ne 0 ]; then
     echo -e "$R ERROR:: Please run this script with root privilege $N"
     exit 1
 fi
- 
+
+mkdir -p "$LOGS_FOLDER"
+echo -e "Script started executed at: $(date) " | tee -a "$LOG_FILE"
+
 USAGE(){
-    echo"USAGE:: sudo sh 24-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS>[optional],default 14 days]"
+    echo "USAGE:: sudo sh 24-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS>[optional, default 14 days]"
     exit 1
 }
 
 if [ $# -lt 2 ]; then
    USAGE
 fi
+
+DAYS=${3:-14}   # default 14 days if not provided
