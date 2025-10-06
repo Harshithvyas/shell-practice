ec4f17808ab3ac3312c68274fa34e277d2886469
Harshith Vyas <harshithvyas@gmail.com>
2025-10-06 12:04:21 +0530

    session-19

--- 24-backup.sh
+++ 24-backup.sh
@@ -5,6 +5,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+SOURCE_DIR=$1
+DEST_DIR=$2
 
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(basename "$0" .sh)
@@ -26,5 +28,12 @@ USAGE(){
 if [ $# -lt 2 ]; then
    USAGE
 fi
+if [ -d $SOURCE_DIR ]; then
+   echo -e "$R $SOURCE_DIR does not exist $N"
+   exit
+fi
 
-DAYS=${3:-14}   # default 14 days if not provided
+if [ ! -d $DEST_DIR ]; then
+   echo -e "$R $SOURCE_DIR does not exist $N"
+   exit
+fi   
