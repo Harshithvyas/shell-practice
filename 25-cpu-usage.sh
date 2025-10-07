bb08ca1329a2a8c451f33d652faf38a9f2a402d2
Harshith Vyas <harshithvyas@gmail.com>
2025-10-07 07:55:11 +0530

    session-20

--- 25-cpu-usage.sh
+++ 25-cpu-usage.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
+DISK_THRESHOLD=2  # in project we keep it as 75%
 
 while IFS= read -r line
 do
-  echo "line: $line"
+  USAGE=$(echo "$line" | awk '{print $6}' | cut -d "%" -f1)
+  MOUNT_POINT=$(echo "$line" | awk '{print $7}')
+
+  if [ "$USAGE" -ge "$DISK_THRESHOLD" ]; then
+    echo "High disk usage detected on $MOUNT_POINT: ${USAGE}%"
+  else
+    echo "Disk usage on $MOUNT_POINT is normal: ${USAGE}%"
+  fi
 done <<< "$DISK_USAGE"
